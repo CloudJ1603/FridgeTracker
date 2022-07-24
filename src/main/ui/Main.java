b212ca999d0e122830405b340a7faaa4056ac54e
lujia <cloud01@students.cs.ubc.ca>
2022-07-23 19:08:12 -0700

    phase 1 draft v5

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,10 @@
 package ui;
 
+// main where the fridge track can be run
 public class Main {
+    // main
     public static void main(String[] args) {
+        // run the fridge tracker application
         new FridgeTracker();
     }
 }
