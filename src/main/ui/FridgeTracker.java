61aa1b5a0346cfe1fcbdffe4745b23c769c0c223
lujia <cloud01@students.cs.ubc.ca>
2022-07-26 16:58:36 -0700

    save and load functions are realized in FridgeTest

--- src/main/ui/FridgeTracker.java
+++ src/main/ui/FridgeTracker.java
@@ -4,12 +4,14 @@ import model.*;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 // fridge tracker application
 public class FridgeTracker {
     // field and constants
-    private static final String JSON_STORE = "./data/myFridge.json";
+    private static final String JSON_STORE = "./data/myFridgeOne.json";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
     private Fridge fridge;
@@ -17,8 +19,13 @@ public class FridgeTracker {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
 
-    // EFFECTS: runs the tacker application
+    // EFFECTS: initializes and runs the tacker application
     public FridgeTracker() {
+        fridge = new Fridge("my Fridge One");
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
         runTracker();
     }
 
@@ -28,7 +35,6 @@ public class FridgeTracker {
     private void runTracker() {
         boolean keepGoing = true;
         String command = "";
-        init();
 
         while (keepGoing) {
             if (!command.equals("n")) {
@@ -47,19 +53,24 @@ public class FridgeTracker {
         System.out.println("\nGoodbye!");
     }
 
-
     // MODIFIES: this
     // EFFECTS: processes user command
     private void processCommand(String command) {
         switch (command) {
             case "a":
-                doAddFood();
+                doAddFood(command);
                 break;
             case "d":
                 doDiscard();
                 break;
             case "r":
-                doRemove();
+                doRemove(command);
+                break;
+            case "s":
+                doSave();
+                break;
+            case "l":
+                doLoad();
                 break;
             case "n":
                 doNextDay(command);
@@ -67,48 +78,67 @@ public class FridgeTracker {
             default:
                 System.out.println("Selection not valid...");
                 break;
-
         }
-    }
-
-    // MODIFIES: this
-    // EFFECTS: initializes fridge
-    private void init() {
-        fridge = new Fridge("myFridge");
-        input = new Scanner(System.in);
-        input.useDelimiter("\n");
     }
 
     // EFFECTS: displays the main menu
     private void displayMenu() {
         System.out.println("\nMain Menu: Select from the following options");
         System.out.println("\ta -> add new food item to the fridge tracker");
-        System.out.println("\tr -> remove food item from  the fridge tracker");
+        System.out.println("\tr -> remove food item from the fridge tracker");
         System.out.println("\td -> discard the expired food");
+        System.out.println("\ts -> save fridge tracker data to file");
+        System.out.println("\tl -> load fridge tracker date from file");
         System.out.println("\tq -> quit");
         System.out.println("\t-----------------------------------------------");
         System.out.println("\tn -> forward to next day");
     }
 
+    // EFFECTS: show sub-menu-forward
+    private void showSubMenuForward() {
+        System.out.println("\nSelect from the following options");
+        System.out.println("\tn -> keep forwarding");
+        System.out.println("\to -> return to the main menu");
+    }
+
+    // EFFECTS: show sub-menu-add
+    private void showSubMenuAdd() {
+        System.out.println("\nSelect from the following options");
+        System.out.println("\ta -> keep adding");
+        System.out.println("\to -> return to the main menu");
+    }
+
+    // EFFECTS: show sub-menu-remove
+    private void showSubMenuRemove() {
+        System.out.println("\nSelect from the following options");
+        System.out.println("\tr -> keep removing");
+        System.out.println("\to -> return to the main menu");
+    }
+
     // MODIFIES: this
     // EFFECTS: add new food item to the fridge
-    private void doAddFood() {
+    private void doAddFood(String cmd) {
+        while (cmd.equals("a")) {
+            Food newFood = instantiateFood();
+            fridge.putInFridge(newFood);
+            System.out.println("\n" + newFood.getName().toUpperCase() + " has been added to the fridge tracker.");
+            showSubMenuAdd();
+            cmd = input.next();
+        }
+    }
 
+    // EFFECTS: instantiates new Food item
+    private Food instantiateFood() {
         Category category = enterCategory();
         String name = enterName();
         int remaining = enterExpiry();
-
-        Food newFood = new Food(name, category, remaining);
-
-        fridge.putInFridge(newFood);
-        System.out.println("\n" + newFood.getName().toUpperCase() + " has been added to the fridge tracker.");
+        return new Food(name, category, remaining);
     }
-
 
     // REQUIRES: input must be one of "v","f","m","l"
     // EFFECTS: displays options for user to enter type of the food item added
     private Category enterCategory() {
-        System.out.println("Please select a category for the food");
+        System.out.println("\nPlease select a category for the food");
 
         int menuLabel = 1;
         for (Category c : Category.values()) {
@@ -118,8 +148,6 @@ public class FridgeTracker {
 
         int menuSelection = input.nextInt();
         return Category.values()[menuSelection - 1];
-
-
     }
 
     // REQUIRES: input must be on non-empty string
@@ -128,7 +156,6 @@ public class FridgeTracker {
         System.out.println("\nEnter the name of item to put in the fridge: ");
         return input.next();
     }
-
 
     // REQUIRES: input must be a positive integer
     // EFFECTS: displays options for user to enter remaining days of the food item added
@@ -186,26 +213,30 @@ public class FridgeTracker {
     // REQUIRED: the current fridge track must contain the name of the item to remove
     // MODIFIES: this
     // EFFECTS: remove the item with the same name as user input from the fridge track
-    private void doRemove() {
-        System.out.println("\nEnter the exact name of the item to be removed: ");
-        String itemToRemove = input.next();
-        Food temp = null;
-        List<Food> foodToRemove = new ArrayList<>();
-        for (int i = 0; i < fridge.getFoodList().size(); i++) {
-            temp = fridge.getFoodList().get(i);
-            if (temp.getName().equals(itemToRemove)) {
-                foodToRemove.add(temp);
-                break;
+    private void doRemove(String cmd) {
+        while (cmd.equals("r")) {
+            System.out.println("\nEnter the exact name of the item to be removed: ");
+            String itemToRemove = input.next();
+            Food temp = null;
+            List<Food> foodToRemove = new ArrayList<>();
+            for (int i = 0; i < fridge.getFoodList().size(); i++) {
+                temp = fridge.getFoodList().get(i);
+                if (temp.getName().equals(itemToRemove)) {
+                    foodToRemove.add(temp);
+                    break;
+                }
             }
-        }
 
-        if (foodToRemove.isEmpty()) {
-            System.out.println("\nNo such food exists in current fridge tracker!");
-        } else {
-            fridge.remove(foodToRemove);
-            System.out.println("\n" + temp.getName().toUpperCase() + " has been removed from the fridge tracker.");
-        }
+            if (foodToRemove.isEmpty()) {
+                System.out.println("\nNo such food exists in current fridge tracker!");
+            } else {
+                fridge.remove(foodToRemove);
+                System.out.println("\n" + temp.getName().toUpperCase() + " has been removed from the fridge tracker.");
+            }
 
+            showSubMenuRemove();
+            cmd = input.next();
+        }
     }
 
     // MODIFIES: this
@@ -214,15 +245,32 @@ public class FridgeTracker {
         while (cmd.equals("n")) {
             fridge.nextDay();
             doShowFoodList();
-            showSubMenu();
+            showSubMenuForward();
             cmd = input.next();
         }
     }
 
-    // EFFECTS: show sub-menu
-    private void showSubMenu() {
-        System.out.println("\n\tn -> keep forwarding");
-        System.out.println("\to -> return to the main menu");
+    /* ----------------- save and load ---------------------------*/
+    // EFFECTS: save the fridge tracker data to file
+    private void doSave() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(fridge);
+            jsonWriter.close();
+            System.out.println("Saved " + fridge.getName() + " to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
+        }
     }
 
+    // MODIFIES: this
+    // EFFECTS: load fridge tracker data from file
+    private void doLoad() {
+        try {
+            fridge = jsonReader.read();
+            System.out.println("Loaded " + fridge.getName() + " from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
+    }
 }
